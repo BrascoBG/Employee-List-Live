9c84e44fddb4ba8b7457b04fb1d18da9d714904b
Ivo Krastev <ivo.krustev@yahoo.com>
2020-08-20 09:19:46 +0300

    Updates

---
+++ precache-manifest.a32f332d525f20bbe27c85a663eede2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0064423823cc4f5f9a71b763e3fa9a46",
+    "url": "/Employee-List-Live/index.html"
+  },
+  {
+    "revision": "fd56888bf1b3970b1614",
+    "url": "/Employee-List-Live/static/css/main.95360745.chunk.css"
+  },
+  {
+    "revision": "a51bfddf215f1f27e774",
+    "url": "/Employee-List-Live/static/js/2.b0042fc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-List-Live/static/js/2.b0042fc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd56888bf1b3970b1614",
+    "url": "/Employee-List-Live/static/js/main.f4cd3775.chunk.js"
+  },
+  {
+    "revision": "919dc2c9afe8d919055b",
+    "url": "/Employee-List-Live/static/js/runtime-main.7ac7e541.js"
+  }
+]);
